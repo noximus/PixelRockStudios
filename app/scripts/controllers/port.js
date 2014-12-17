a73328a7a4567150939569a26defad93cd52653c
noximus <hrichardson83@gmail.com>
2014-12-17 02:50:11 -0500

    made button sound universal

--- app/scripts/controllers/port.js
+++ app/scripts/controllers/port.js
@@ -20,6 +20,7 @@ angular.module('pixelRockStudiosApp')
     TweenMax.to(navBG, 1, {css:{'margin-left':"137px"}, ease:Bounce.easeOut});
     TweenMax.to(breadCrumb, 1, {css:{'padding-top':"10px",'height':"30px",'display':'block','opacity':'1'}, ease:Bounce.easeOut});
     $(".pixel-button").hover(function(e){
+    	mouseoversound.playclip();
     	TweenMax.to($(this).children('img'), 1, {css:{'margin-right':"30px"}, ease:Elastic.easeOut});
     }, function(e){
     	TweenMax.to($(this).children('img'), 1, {css:{'margin-right':"5px"}, ease:Bounce.easeOut});
@@ -37,5 +38,6 @@ angular.module('pixelRockStudiosApp')
     	//console.log($("#launchGit").data("link"));
     	window.open($("#launchSite").data("link"));
     });
+
 
   });
