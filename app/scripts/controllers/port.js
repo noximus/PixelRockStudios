0fd41166136539fe4238fa5f3173d2c5b6c5b45d
noximus <hrichardson83@gmail.com>
2014-12-18 00:50:35 -0500

    fixing port

--- app/scripts/controllers/port.js
+++ app/scripts/controllers/port.js
@@ -14,16 +14,52 @@ angular.module('pixelRockStudiosApp')
       'AngularJS',
       'Karma'
     ];
+    $scope.portfolio = [
+    {'name': 'BMW M 40th Anniversary test',
+     'link': 'http://www.bmwusa.com/m'},
+    {'name': 'BMW Motorsports',
+     'link': 'http://www.bmwusa.com/standard/content/explore/bmwenthusiasts/bmwmotorsports.aspx'},
+    {'name': 'BMW ConnectedDrive',
+     'link': 'http://www.bmwusa.com/standard/content/innovations/bmwconnecteddrive/connecteddrive.aspx'},
+    {'name': 'BMW Performance Driving School',
+     'link': 'http://www.bmwusa.com/pds'},
+    {'name': 'Justin Biebers Collection',
+     'link': 'http://www.justinbiebercollectorsedition.com/#home'},
+    {'name': 'Biore Skincare',
+     'link': 'http://www.biore.com/en-US/biore-skincare'},
+    {'name': 'BMW i8 Experience',
+     'link': 'http://www.bmwusa.com/standard/content/vehicles/2014/bmwi/bmwi_i8redirect.aspx?from=/Standard/Content/Vehicles/2014/BMWi/BMWi8RD.aspx&return=/Standard/Content/Vehicles/2014/BMWi/BMWi8RD.aspx'},
+    {'name': 'Fantastic 4 Rise of the Silver Surfer',
+     'link': 'http://www.pixelrockstudios.com/work/f4'},
+    {'name': 'Chevy Flash Calculator',
+     'link': 'http://www.pixelrockstudios.com/work/chevy'},
+    {'name': 'Ritz Carlton White Plains',
+     'link': 'http://www.pixelrockstudios.com/work/ritz'},
+    {'name': 'Kodak Flash Banner',
+     'link': 'http://www.pixelrockstudios.com/work/kodak'},
+    {'name': 'Crestor Astra Zeneca',
+     'link': 'http://www.pixelrockstudios.com/work/astra'}
+    // ,
+    // {'name': 'ConnectedDrive',
+    //  'link': 'Generationtablet'},
+    // {'name': 'ConnectedDrive',
+    //  'link': 'Generationtablet'},
+    // {'name': 'ConnectedDrive',
+    //  'link': 'Generationtablet'},
+    // {'name': 'ConnectedDrive',
+    //  'link': 'Generationtablet'}
+  ];
     var navBG = $(".navBg");
     var breadCrumb = $(".bread-crumbs");
     var buttons = $(".pixel-button img");
     TweenMax.to(navBG, 1, {css:{'margin-left':"137px"}, ease:Bounce.easeOut});
     TweenMax.to(breadCrumb, 1, {css:{'padding-top':"10px",'height':"30px",'display':'block','opacity':'1'}, ease:Bounce.easeOut});
-    $(".pixel-button").hover(function(e){
-    	mouseoversound.playclip();
-    	TweenMax.to($(this).children('img'), 1, {css:{'margin-right':"30px"}, ease:Elastic.easeOut});
-    }, function(e){
-    	TweenMax.to($(this).children('img'), 1, {css:{'margin-right':"5px"}, ease:Bounce.easeOut});
+    $('body').on('mouseover', '.pixel-button', function(e){
+      console.log("hovering");
+      TweenMax.to($(this).children('img'), 1, {css:{'margin-right':"30px"}, ease:Elastic.easeOut});
+    });
+    $('body').on('mouseout', '.pixel-button', function(e){
+      TweenMax.to($(this).children('img'), 1, {css:{'margin-right':"5px"}, ease:Bounce.easeOut});
     });
     // Temporary button to link to site before adding angular database
     $(".pixel-button").click(function(e){
