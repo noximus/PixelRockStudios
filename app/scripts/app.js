e4ded752f335b1173434426746809185ea1476a8
noximus <hrichardson83@gmail.com>
2015-03-01 16:54:13 -0500

    configured ui-router and angular-breadcrumbs

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -14,37 +14,60 @@ angular
     'ngAnimate',
     'ngCookies',
     'ngResource',
-    'ngRoute',
+    'ui.router',
     'ngSanitize',
     'ngTouch',
     'ncy-angular-breadcrumb'
   ])
-  .config(function ($routeProvider) {
-    $routeProvider
-      .when('/', {
-        templateUrl: 'views/main.html',
-        controller: 'MainCtrl'
-      })
-      .when('/portfolio', {
-        templateUrl: 'views/portfolio.html',
+  .config(function($stateProvider, $urlRouterProvider) {
+  //
+  // For any unmatched url, redirect to /state1
+  $urlRouterProvider.otherwise("/home");
+  //
+  // Use angular-breadcrumbs for this *requires ui-router replacing ngRoute
+  // https://github.com/ncuillery/angular-breadcrumb
+  $stateProvider
+    .state('home', {
+      url: "/home",
+      templateUrl: "views/main.html"
+    })
+    .state('portfolio', {
+      url: "/portfolio",
+      templateUrl: "views/portfolio.html",
         controller: 'PortCtrl',
         ncyBreadcrumb: {
           label: 'Portfolio'
         }
-      })
-      .when('/experimental', {
-        templateUrl: 'views/experimental.html',
-        controller: 'ExperimentalCtrl'
-      })
-      .when('/contact', {
-        templateUrl: 'views/contact.html',
-        controller: 'ContactCtrl'
-      })
-      .when('/bio', {
-        templateUrl: 'views/bio.html',
-        controller: 'BioCtrl'
-      })
-      .otherwise({
-        redirectTo: '/'
-      });
-  });
+    })
+    //   .state('portfolio.list', {
+    //     url: "/list",
+    //     templateUrl: "views/portfolio.details.html",
+    //     controller: function($scope) {
+    //       $scope.things = ["A", "Set", "Of", "Things"];
+    //     }
+    // })
+    .state('bio', {
+      url: "/bio",
+      templateUrl: "views/bio.html",
+        controller: 'BioCtrl',
+        ncyBreadcrumb: {
+          label: 'Bio'
+        }
+    })
+    .state('experimental', {
+      url: "/experimental",
+      templateUrl: "views/experimental.html",
+        controller: 'ExperimentalCtrl',
+        ncyBreadcrumb: {
+          label: 'Experimental'
+        }
+    })
+    .state('contact', {
+      url: "/bio",
+      templateUrl: "views/bio.html",
+        controller: 'ContactCtrl',
+        ncyBreadcrumb: {
+          label: ''
+        }
+    });
+});
