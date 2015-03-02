a6b84abe6d336ea89301f7c6dd927911d7d3e549
noximus <hrichardson83@gmail.com>
2015-03-02 11:06:35 -0500

    blah

--- app/scripts/directives/navigation.js
+++ app/scripts/directives/navigation.js
@@ -6,7 +6,7 @@
  * @description
  * # navigation
  */
-angular.module('pixelRockStudiosApp')
+angular.module('pixelRockStudiosApp',$rootScope)
   .directive('navigation', function () {
     return {
       templateUrl: '../views/partials/navigation.html',
@@ -14,9 +14,15 @@ angular.module('pixelRockStudiosApp')
       replace:true,
       link: function postLink(scope, element, attrs) {
         // element.text('this is the navigation directive');
+        //$rootScope.nav.items = ['home','bio','portfolio','contact'];
+        //console.log($rootScope.nav.items[0]);
         // console.log(scope, element, attrs);
 
               // add navigation controller to this level
+
+              // 1 move nav elements first
+              // 2 convert to OOP after
+
 
       }
     };
