99e0c7282aa914557c3a2625c9c22b5c4d09c805
noximus <hrichardson83@gmail.com>
2015-03-03 13:09:33 -0500

    adding comments to fix previous section and make MVC compliant

--- app/scripts/directives/navigation.js
+++ app/scripts/directives/navigation.js
@@ -9,9 +9,11 @@ angular.module('pixelRockStudiosApp')
       	scope.navigationFact = navigationFact;
           // add navigation controller to this level - check
         scope.navClicked = function(item){
+
+        	// proper MVC framework requires us to first add this to the model
         	var navBG = $(".navBg");
-        	var newOffset = (122*item)+15;//needs a negative offset to compensate for graphics
-        	// console.log("New offset =", newOffset);
+        	var newOffset = (122*item)+15;
+        	// This animation should be in the "VIEW" and should be listening for the MODEL to change
         	TweenMax.to(navBG, 1, {css:{'margin-left':newOffset+"px"}, ease:Bounce.easeOut});
         }
         
