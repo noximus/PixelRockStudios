bc541ca56befd3c40f7210889dedc167bd0905cd
noximus <hrichardson83@gmail.com>
2015-03-03 12:46:56 -0500

    added id to navigation to get offset.left for animations.

--- app/scripts/directives/navigation.js
+++ app/scripts/directives/navigation.js
@@ -8,7 +8,10 @@ angular.module('pixelRockStudiosApp')
       link: function postLink(scope, element, attrs) {
       	scope.navigationFact = navigationFact;
           // add navigation controller to this level - check
-
+        scope.navClicked = function(item){
+        	console.log("element find attr",element.find('li#'+item).offset().left);
+        }
+        
           // 1 move nav elements first 
           // 2 convert to OOP after
       }
