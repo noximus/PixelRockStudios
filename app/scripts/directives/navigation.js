80b3ffb0bd1525c170a5515a178db79832ae3f8e
noximus <hrichardson83@gmail.com>
2015-03-03 11:37:26 -0500

    cleaned up and changes navigation factory js file name

--- app/scripts/directives/navigation.js
+++ app/scripts/directives/navigation.js
@@ -1,11 +1,4 @@
 'use strict';
-
-/**
- * @ngdoc directive
- * @name pixelRockStudiosApp.directive:navigation
- * @description
- * # navigation
- */
 angular.module('pixelRockStudiosApp')
   .directive('navigation', function (navigationFact) {
     return {
@@ -14,24 +7,10 @@ angular.module('pixelRockStudiosApp')
       replace:true,
       link: function postLink(scope, element, attrs) {
       	scope.navigationFact = navigationFact;
-      	// console.log(scope.navigationFact);
-      	// scope.activeIndex=0;
-      	element.children().children().click(function(){
-      		
-	    	// $(".navbut").removeClass("active");
-	    	// $(this).children().addClass("active");
-	    });
-        // element.text('this is the navigation directive');
-        //$rootScope.nav.items = ['home','bio','portfolio','contact'];
-        //console.log($rootScope.nav.items[0]);
-        // console.log(scope, element, attrs);
+          // add navigation controller to this level - check
 
-              // add navigation controller to this level
-
-              // 1 move nav elements first
-              // 2 convert to OOP after
-
-
+          // 1 move nav elements first 
+          // 2 convert to OOP after
       }
     };
   });
