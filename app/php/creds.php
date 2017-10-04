9184e3102633b5d07547e85cc46a9ae9dbd48f43
Henry Richardson <hrichardson83@gmail.com>
2017-10-04 11:10:33 -0400

    added scroll bar coloring

---
+++ app/php/creds.php
@@ -0,0 +1,17 @@
+<?php
+  // Email, this is for PHPMAILER to works properly
+  define('EMAIL', 'emailsubmission@pixelrockstudios.com');
+  define('EMAIL_PASSWORD', "':B!2QSVXHsvAD");
+  define('EMAIL_HOST', 'box805.bluehost.com');
+  define('EMAIL_PORT', '465');
+  define('EMAIL_SMPTSecure', 'ssl');
+
+  // Marker API Sercret DO NOT SHARE
+  define('APIKey', '555b96c8e4b08787329ffd14');
+  define('APISecret', 'BqNvot4rcEG58D3n2a3y1lzN9kk');   
+
+  // Database, this is for database interaction
+  define('DATABASE', 'database_name');
+  define('USERNAME', 'database_username');
+  define('PASSWORD', 'database_password');
+?>
